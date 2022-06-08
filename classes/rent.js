e1727020d05c1c79cbf691874f116e1200c1737b
afanasievarin <afanasevarin2001@mail.ru>
2022-06-08 19:34:07 +0400

    добавление в паршл

--- classes/rent.js
+++ classes/rent.js
@@ -30,4 +30,23 @@ async function getRentableItems(){
     return rentableitems[0];
 };
 
-module.exports = {getRentableItems};
+async function getItemByID(ID){
+    let item = await pool.execute(`
+        SELECT * FROM items
+        INNER JOIN categories 
+        ON items.categoryID = categories.categoryID
+        INNER JOIN itemstatuses 
+        ON items.itemstatusID = itemstatuses.itemstatusID
+        INNER JOIN itemconditions 
+        ON items.itemconditionID = itemconditions.itemconditionID
+        WHERE items.itemID = "${ID}"
+        LIMIT 1
+    `)
+    .catch(err =>{
+        console.log(err);
+    });
+    //console.log(items[0]);
+    return item[0];
+}
+
+module.exports = {getRentableItems, getItemByID};
--- routes/rentroutes.js
+++ routes/rentroutes.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const {getRentableItems} = require("../classes/rent.js");
+const {getRentableItems, getItemByID} = require("../classes/rent.js");
 
 router.get("/rent", async function(_,response){
     var rentableitems = await getRentableItems();
     response.render("rent.hbs", {items: rentableitems});
     //console.log(items);
-  });
+});
+
+router.get("/rentitempage/:id", async function(request,response){
+  var id = request.params.id;
+  var item = await getItemByID(id);
+  response.render("rentitempage.hbs", {item: item[0]});
+  console.log(item);
+});
 
 module.exports = router;
